2d978ca5193975a51736bc349477ebd78371f9c7
Subhashish Nabajja <subhashishnabajja1575643@gmail.com>
2025-04-20 23:16:43 +0530

    chore: Parked all the rest of the page.

--- public/creative-mighty/assets/js/main.js
+++ public/creative-mighty/assets/js/main.js
@@ -2685,7 +2685,7 @@
     countDown: function (e) {
       if ($("#countdown-time").length) {
         // Set the date we're counting down to
-        var countDownDate = new Date("2025-05-12 12:00").getTime();
+        var countDownDate = new Date("2025-05-01T16:00:00Z").getTime();
 
         // Update the count down every 1 second
         var x = setInterval(function () {
